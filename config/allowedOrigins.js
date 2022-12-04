721c12cba02cd179ae2e12b8a62231c1b4a9602c
sz-chi <judy881204@gmail.com>
2022-12-04 23:57:23 +0800

    first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,22 @@
+const allowedOrigins = [
+    'http://127.0.0.1:5000',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:3000/Login',
+    'https://longing-head-production.up.railway.app/Login',
+    'https://longing-head-production.up.railway.app',
+    'https://reactproject3client-production.up.railway.app',
+    'https://reactproject3client-production.up.railway.app/',
+    'https://reactproject3client-production.up.railway.app/Article/',
+    'https://reactproject3client-production.up.railway.app/Article2/',
+    'https://reactproject3client-production.up.railway.app/Article/1',
+    'https://reactproject3client-production.up.railway.app/Article/2',
+    'https://reactproject3client-production.up.railway.app/Article/3',
+    'https://reactproject3client-production.up.railway.app/Article/4',
+    'https://reactproject3client-production.up.railway.app/Article2/1',
+    'https://reactproject3client-production.up.railway.app/Article2/2',
+    'https://reactproject3client-production.up.railway.app/Article2/3',
+    'https://reactproject3client-production.up.railway.app/Article2/4',
+];
+
+module.exports = allowedOrigins;
